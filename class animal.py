4b571ace5251d80de802ea171b6ad7d8ee6c2912
Vall <samarabvr@gmail.com>
2018-11-21 15:00:40 +0400

    new1'

--- class animal.py
+++ class animal.py
@@ -1,46 +1,46 @@
 class Animal:
     def __init__(self, name, weight):
         self.name = name
-	    self.weight = weight
+        self.weight = weight
 
-	def eat(self):
+    def eat(self):
         print('{0} накормлен'.format(self.name))
 	
 
     def say(self):
-    pass
+        pass
     def get(self):
-    pass
+        pass
 
 class Korova(Animal):
     def say(self):
         print('{0} сказала Myyy'.format(self.name))
     def get(self):
-        print('{0) подоена'.format(self.name))
+        print('{0} подоена'.format(self.name))
 
 class Gus(Animal):
     def say(self):
         print('{0} сказала Гыл-Гыл'.format(self.name))
     def get(self):
-        print('Собраны яйца у {0)'.format(self.name))
+        print('Собраны яйца у {0}'.format(self.name))
 
 class Sheep(Animal):
     def say(self):
         print('{0} сказала Бее'.format(self.name))
     def get(self):
-        print('{0) подстрижена'.format(self.name))
+        print('{0} подстрижена'.format(self.name))
 
-class Utka(Amimal):
+class Utka(Animal):
     def say(self):
         print('{0} сказала Кря-Кря'.format(self.name))
     def get(self):
-        print('Собраны яйца у {0)'.format(self.name))
+        print('Собраны яйца у {0}'.format(self.name))
 
 class Chiken(Animal):
     def say(self):
         print('{0} сказала Ко-Ко'.format(self.name))
     def get(self):
-        print('Собраны яйца у {0)'.format(self.name))
+        print('Собраны яйца у {0}'.format(self.name))
 
 class Goat(Animal):
     def say(self):
@@ -63,11 +63,17 @@ all_animals = [gus1, gus2, korova, sheep1, sheep2, utka, chiken1, chiken2, goat1
 
 weight = 0
 for animal in all_animals:
-        weight += animal.weight
-        animal.eat()
-        animal.say()
-        animal.get()
-print(weight)
+    weight += animal.weight
+    animal.eat()
+    animal.say()
+    animal.get()
+
+print('Общий вес животных:', weight, 'кг.')
+
+print('Самое тяжелое животное:', max(all_animals).weight)
+
+
+
         
         
         
---
+++ cookbook.py
@@ -0,0 +1,28 @@
+book = open('cookbook.txt').read().split('\n\n')
+cook_book = {}
+for i in book:
+     i = i.split('\n')
+     cook_book[i[0]] = []
+     for j in i[2:]:
+          j = j.split(' | ')
+          if not j[0]: break
+          cook_book[i[0]].append({'ingridient_name': j[0], 'quantity': int(j[1]), 'measure': j[2]})
+
+print(cook_book)
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+     out = {}
+     for i in dishes:
+          try:
+               for i in cook_book[i]:
+                    
+                    out[i['ingridient_name']] = {}
+                    out[i['ingridient_name']]['measure'] = i['measure']
+                    out[i['ingridient_name']]['quantity'] = i['quantity'] *2
+          except KeyError:
+               print('Блюда не существует')
+
+     return(out)
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))

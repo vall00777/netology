ab2d8e5656b265e997c7e9859feaff7def7aea27
Валерий <36442895+vall00777@users.noreply.github.com>
2018-11-21 15:05:14 +0400

    Delete 12.py

--- 12.py
+++
@@ -1,9 +0,0 @@
-znak, a, b = input().split()
-if   znak == '+':
-     print(float(a)+float(b))
-elif znak == '-':
-     print(float(a)-float(b))
-elif znak == '*':
-     print(float(a)*float(b))
-elif znak == '/':
-     print(float(a)/float(b))

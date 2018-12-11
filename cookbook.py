a214e1ced11e229a9dde31dc154705bfb9c99524
Валерий <36442895+vall00777@users.noreply.github.com>
2018-12-11 16:07:29 +0300

    Update cookbook.py

--- cookbook.py
+++ cookbook.py
@@ -9,20 +9,16 @@ def get_cookbook(file):
                if not j[0]: break
                cook_book[i[0]].append({'ingridient_name': j[0], 'quantity': int(j[1]), 'measure': j[2]})
                return cook_book
-
-
 def get_shop_list_by_dishes(dishes, person_count):
      out = {}
      for i in dishes:
           try:
                for i in cook_book[i]:
-                    
                     out[i['ingridient_name']] = {}
                     out[i['ingridient_name']]['measure'] = i['measure']
                     out[i['ingridient_name']]['quantity'] = i['quantity'] *2
           except KeyError:
                print('Блюда не существует')
-
-     return(out)
-
+          return(out)
 print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
